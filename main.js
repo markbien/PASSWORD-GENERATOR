3cf6dba3e734835007623a5640c0b2746b4b7e77
Mark Bien <48498353+markbien@users.noreply.github.com>
2024-12-29 15:20:52 +0800

    Add DomHandler

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const passwordGenerator = (()=> {
+const PasswordGenerator = (() => {
   const lowercase = "abcdefghijklmnopqrstuvwxyz";
   const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   const numbers = "0123456789";
@@ -12,7 +12,7 @@ const passwordGenerator = (()=> {
     allowedChars += uppercase;
     allowedChars += numbers;
     allowedChars += symbols;
-    
+
     for (let i = 0; i < 12; i++) {
       const randomIndex = Math.floor(Math.random() * allowedChars.length);
       newPassword += allowedChars[randomIndex];
@@ -22,4 +22,46 @@ const passwordGenerator = (()=> {
   }
 
   return { generatePassword };
+})();
+
+const DomHandler = (() => {
+  const passwordLength = () => {
+    const passwordLengthDom = document.querySelector("#password-length");
+    return parseInt(passwordLengthDom.value);
+  };
+
+  const isLowerCaseIncluded = () => {
+    const lowercaseDom = document.querySelector("#lowercase");
+    return lowercaseDom.checked;
+  };
+
+  const isUpperCaseIncluded = () => {
+    const uppercaseDom = document.querySelector("#uppercase");
+    return uppercaseDom.checked;
+  };
+
+  const isNumberIncluded = () => {
+    const numberDom = document.querySelector("#numbers");
+    return numberDom.checked;
+  }
+
+  const isSymbolIncluded = () => {
+    const symbolDom = document.querySelector("#symbols");
+    return symbolDom.checked;
+  }
+
+  return {
+    passwordLength,
+    isLowerCaseIncluded,
+    isUpperCaseIncluded,
+    isNumberIncluded,
+    isSymbolIncluded,
+  };
+})();
+
+const Main = (() => {
+  const generateBtn = document.querySelector('#generatePassword');
+  generateBtn.addEventListener('click', function(){
+    console.log("Hello");
+  });
 })();
